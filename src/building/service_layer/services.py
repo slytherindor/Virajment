4c4aaeaa4280583bf3a60070d3677d7632275d31
Shubhanshu <shubh.aarsay@gmail.com>
2020-10-30 02:33:37 -0400

    Added 3 new function for adding new data.

--- src/building/service_layer/services.py
+++ src/building/service_layer/services.py
@@ -1,13 +1,28 @@
-from building.adapters.repository import AbstractRepository
-from building.domain.model import Building, BuildingSection
+from building.adapters.repository import AbstractBuildingRepository, AbstractDataRepository
+from building.domain.model import Building, BuildingSection, LeadData, PcbData, AcmData
 
 
-def create_building(building: Building, repo: AbstractRepository, session):
+def create_building(building: Building, repo: AbstractBuildingRepository, session):
     repo.add(building)
     session.commit()
 
 
-def create_building_section(building_section: BuildingSection, repo: AbstractRepository, session):
+def create_building_section(building_section: BuildingSection, repo: AbstractBuildingRepository, session):
     building = repo.get(building_section.building_id)
     building.building_sections.add(building_section)
+    session.commit()
+
+
+def create_lead_data(data: LeadData, repo: AbstractDataRepository, session):
+    repo.add(data)
+    session.commit()
+
+
+def create_pcb_data(data: PcbData, repo: AbstractDataRepository, session):
+    repo.add(data)
+    session.commit()
+
+
+def create_acm_data(data: AcmData, repo: AbstractDataRepository, session):
+    repo.add(data)
     session.commit()
