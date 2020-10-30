3a3823e69f5712af4608785567fd260400154569
Shubhanshu <shubh.aarsay@gmail.com>
2020-10-30 02:11:26 -0400

    Renamed functions according to pep8

--- src/building/service_layer/services.py
+++ src/building/service_layer/services.py
@@ -2,12 +2,12 @@ from building.adapters.repository import AbstractRepository
 from building.domain.model import Building, BuildingSection
 
 
-def createBuilding(building: Building, repo: AbstractRepository, session):
+def create_building(building: Building, repo: AbstractRepository, session):
     repo.add(building)
     session.commit()
 
 
-def createBuildingSection(building_section: BuildingSection, repo: AbstractRepository, session):
+def create_building_section(building_section: BuildingSection, repo: AbstractRepository, session):
     building = repo.get(building_section.building_id)
     building.building_sections.add(building_section)
     session.commit()
