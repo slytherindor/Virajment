ee7c670fefb63b0cd8e1f2d81a565c10ee4ff822
Shubhanshu <shubh.aarsay@gmail.com>
2020-10-30 18:13:14 -0400

    Mistakenly added flask code to init file. moved it to correct file.

--- src/building/entrypoints/flask_app.py
+++ src/building/entrypoints/flask_app.py
@@ -1,2 +1,43 @@
-import flask
+from building.domain.model import Building
+from flask import Flask, jsonify, request, make_response
+from building.service_layer import services
+import uuid
+from building.adapters.repository import SqlAlchemyBuildingRepository
+from building.adapters.orm import metadata, start_mappers
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from building import config
 
+app = Flask(__name__)
+
+
+def init_engine_with_models():
+    engine = create_engine(config.get_postgres_uri())
+    metadata.create_all(engine)
+    return engine
+
+
+engine_1 = init_engine_with_models()
+start_mappers()
+get_session = sessionmaker(bind=engine_1)
+
+
+@app.route("/building", methods=["POST"])
+def add_building():
+    session = get_session()
+    repo = SqlAlchemyBuildingRepository(session)
+    try:
+        building_uuid = uuid.uuid4().hex[:6]
+        building = Building(
+            building_uuid,
+            request.json['name'],
+            request.json['address'],
+            request.json['city'],
+            request.json['zipcode'],
+            request.json['province'],
+        )
+
+        services.create_building(building, repo, session=session)
+    except Exception as e:
+        return jsonify({'message': str(e)}), 400
+    return make_response('Created', 201)
