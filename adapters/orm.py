48e2329b02b240835fe38b0da5322a7639ae3767
Shubhanshu <shubh.aarsay@gmail.com>
2020-10-30 00:32:47 -0400

    added relationship between building section and building

--- adapters/orm.py
+++ adapters/orm.py
@@ -9,11 +9,21 @@ metadata = MetaData()
 building = Table(
     'building', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('building_id', String(255)),
     Column('name', String(255)),
     Column('address', String(255)),
     Column('city', String(255)),
     Column('zipcode', String(255)),
     Column('province', String(255)),
+)
+
+building_section = Table(
+    'building_section', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('building_id', ForeignKey('building.id')),
+    Column('floor', String(255)),
+    Column('room', String(255)),
+    Column('location', String(255)),
 )
 
 acm_data = Table(
@@ -61,22 +71,15 @@ pcb_data = Table(
     Column('notes', String(255)),
 )
 
-building_section = Table(
-    'building_section', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('building_id', ForeignKey('building.id')),
-    Column('floor', String(255)),
-    Column('room', String(255)),
-    Column('location', String(255)),
-)
 
 def start_mappers():
     building_section_mapper = mapper(model.BuildingSection, building_section)
     mapper(model.Building, building, properties={
-        'building_section': relationship(building_section_mapper)
+        'building_sections': relationship(building_section_mapper,
+                                          collection_class=set)
     })
 
-    acm_data_mapper = mapper(model.AcmData, acm_data)
-    lead_data_mapper = mapper(model.LeadData, lead_data)
-    pcb_data_mapper = mapper(model.PcbData, pcb_data)
+    mapper(model.AcmData, acm_data)
+    mapper(model.LeadData, lead_data)
+    mapper(model.PcbData, pcb_data)
 
